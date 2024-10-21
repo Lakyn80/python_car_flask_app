38cb22efc3f5463db9632fac547ed0a9cb9526bc
Lakyn80 <lukas.krumpach@gmail.com>
2024-10-22 01:27:59 +0300

    adding buttom delete from web and from json

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-#Folder, kde budeme ukládat seznam aut
-CAR_FILE  = "cars.json"
+# Soubor, kde budeme ukládat seznam aut
+CAR_FILE = 'cars.json'
 
-# class for cars
+# Třída pro auta
 class Car:
     def __init__(self, color, model, car_type):
         self.color = color
@@ -15,15 +15,16 @@ class Car:
 
     def describe_car(self):
         return f"This car is {self.color}, model {self.model}, and a type {self.car_type}."
-    
-    #Pomocná metoda pro uložení aut ze slovníku
+
     def to_dict(self):
+        # Pomocná metoda pro uložení auta jako slovník
         return {"color": self.color, "model": self.model, "car_type": self.car_type}
-    
-    #Pomocná metoda pro načtení aut ze slovníku
+
     @staticmethod
     def from_dict(data):
-        return Car(data["color"], data["model"], data["car_type"])
+        # Pomocná metoda pro načtení auta ze slovníku
+        return Car(data['color'], data['model'], data['car_type'])
+
 
 # Funkce pro načtení seznamu aut ze souboru JSON
 def load_cars():
@@ -32,7 +33,7 @@ def load_cars():
             cars_data = json.load(f)
             return [Car.from_dict(car) for car in cars_data]
     except (FileNotFoundError, json.JSONDecodeError):
-        return []   
+        return []
 
 # Funkce pro uložení seznamu aut do souboru JSON
 def save_cars(cars):
@@ -46,8 +47,8 @@ cars = load_cars()
 # Route pro hlavní stránku
 @app.route('/')
 def index():
-    return render_template('index.html', cars=cars)
-
+    cars_with_index = [(index, car) for index, car in enumerate(cars)]
+    return render_template('index.html', cars=cars_with_index)
 
 # Route pro přidání auta
 @app.route('/add_car', methods=['POST'])
@@ -62,6 +63,18 @@ def add_car():
 
     # Ulož seznam aut do souboru
     save_cars(cars)
+
+    return redirect('/')
+
+# Route pro odstranění auta
+@app.route('/delete_car', methods=['POST'])
+def delete_car():
+    index = int(request.form['index'])
+    
+    # Odstraníme auto ze seznamu podle indexu
+    if 0 <= index < len(cars):
+        del cars[index]
+        save_cars(cars)
 
     return redirect('/')
 
